a40c335f771160616332335ae8d72677ce1ea4f2
Suman Sigdel <sumansid1113@gmail.com>
2021-11-13 16:45:04 -0500

    Add codemirror syntax highlighting for JS

--- encod-client/src/components/TeamEditor.js
+++ encod-client/src/components/TeamEditor.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {RouteComponentProps} from "react-router-dom";
 import TextEditor from './TextEditor';
+import Header from './Header';
 
 
 function TeamEditor(props) {
     let id = props.match.params.id;
     return (
         <div>
-        <TextEditor groupId={id}/>
-            
+        <Header/>
+        <TextEditor groupId={id}/> 
         </div>
     )
 }
--- encod-client/src/components/TextEditor.js
+++ encod-client/src/components/TextEditor.js
@@ -1,13 +1,15 @@
 import React,{useState, useEffect} from "react";
 import io from "socket.io-client";
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror from 'react-codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript';
 
 const socket = io.connect("http://localhost:4000/")
 
 function TextEditor(props) {
     const client_group_id = props.groupId;
     
-    const [code, setCode] = useState("start")
+    const [code, setCode] = useState("function helloWorld(){}")
     
     useEffect(() => {
         socket.on(`message-${client_group_id}`, handleCodeArea)
@@ -23,23 +25,28 @@ function TextEditor(props) {
     function sendData(value){
         //console.log("value from components ", value)
         //var res = value.target.value
-
-        var res = value;
         
+        var res = value;
         socket.emit("message", {value : res, groupId : client_group_id})
         setCode(res)
     }
     return (
         <div className="App">
         <h1> Enter code</h1>
-    
+        
         <CodeMirror
-      value={code}
-      height="200px"
-      onChange={(value, viewUpdate) => {
-        sendData(value);
-      }}
-    />
+        
+        value={code}
+        height="200px"
+        options={{
+            lineNumbers: true,
+            readOnly: false,
+            mode: "javascript"
+        }}
+        onChange={(value, viewUpdate) => {
+            sendData(value);
+        }}
+        />
         </div>
         );
     }
