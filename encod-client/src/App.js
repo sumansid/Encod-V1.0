6cbfc14aa98fbfba51deeab86e19965cef663383
Suman Sigdel <sumansid1113@gmail.com>
2021-11-29 11:03:48 -0500

    Add # to window.location.href

--- encod-client/src/App.js
+++ encod-client/src/App.js
@@ -1,26 +1,27 @@
 import './App.css';
-import {BrowserRouter, HashRouter,useHistory, Route, Redirect, withRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter ,useHistory, Route, Redirect, withRouter, Switch} from "react-router-dom";
 import Button from './components/Button';
 import Header from './components/Header';
 import TeamEditor from './components/TeamEditor';
 
 function App() {
 
-  const history = useHistory();
 
   function createRoomButtonHandler(event){
-    window.location.href=`/group/${Date.now()}`
+    window.location.href=`#/group/${Date.now()}`
   }
   return (
 
     <HashRouter>
-      <Route path="/" exact render={() => {
-        return (<div className="App">
-    <Header/>
-    <Button name="Create Room" clickHandler={createRoomButtonHandler}/>
-    </div>)
-      }}></Route>
-      <Route path="/group/:id" component={TeamEditor}></Route>
+      <div>
+        <Route exact path="/" component={() => {
+          return (<div className="App">
+      <Header/>
+      <Button name="Create Room" clickHandler={createRoomButtonHandler}/>
+      </div>)
+        }}/>
+        <Route path="/group/:id" component={TeamEditor}/>
+        </div>
     </HashRouter>
 
 
