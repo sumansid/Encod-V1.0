cd7ee3d1c5cddbd0ad72425801fd2f7df0cfd089
Suman Sigdel <sumansid1113@gmail.com>
2022-10-08 16:50:01 -0400

    Fix code formatting, remove unused variables

--- encod-server/app.js
+++ encod-server/app.js
@@ -1,51 +1,38 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const path = require("path");
-const server = http.createServer(app);
-const roomLookup = {};
 require("dotenv").config();
+
+const server = http.createServer(app);
 const port = process.env.PORT || 4000;
 
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
-})
+    methods: ["GET", "POST"],
+  },
+});
 
 const cors = require("cors");
-const { join } = require("path");
-app.use(cors({
-  origin:"*"
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
+server.listen(port, function () {
+  console.log("listening in port ", port);
+});
 
+io.on("connection", function (client) {
+  console.log("new connection");
 
-server.listen(port, function(){
-  console.log("listening in port ", port)
-})
+  client.on("message", broadcastData);
+  client.on("createNewRoom", createNewRoom);
+  client.on("joinExistingRoom", joinExistingRoom);
 
-io.on('connection', function(client){
-  console.log("new connection")
-  
-  client.on("message", broadcastData)
-  client.on("createNewRoom", createNewRoom)
-  client.on("joinExistingRoom", joinExistingRoom)
-  
-  function broadcastData(data){
-    console.log("data from the client ", data )
-    client.broadcast.emit(`message-${data.groupId}`, data.value)
-    
+  function broadcastData(data) {
+    console.log("data from the client ", data);
+    client.broadcast.emit(`message-${data.groupId}`, data.value);
   }
-  
-  function createNewRoom(){
-    var newRoomID = makeNewRoomID();
-    roomLookup[client.id] = newRoomID;
-  }
-  
-})
-
-
-
-
+});
